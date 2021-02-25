fad637ba31c4e597a11d1623404ae97795432785
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-02-25 11:01:57 -0300

    Add files via upload

---
+++ vuejs-origamid/exemplo_componente/componente/buttton.js
@@ -0,0 +1,8 @@
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">Você clicou em mim {{ count }} vezes.</button>'
+})
